0a486e02426db12d67f5f2b546f9ee17aa443a31
Olaoluwa2170 <elitebabson@gmail.com>
2024-07-06 04:51:14 +0100

    chore: included apikeys cause adaptable did'nt create space for inputting env variables

--- index.js
+++ index.js
@@ -8,31 +8,29 @@ app.get('/api/hello', async (req, res) => {
     const clientIp = req.ip;
 
     // Replace with your IPStack API key
-    const ipstackApiKey = '3603411b489e490fe3f455f0bf710b76';
-    const ipstackUrl = `http://api.ipstack.com/${clientIp}?access_key=${ipstackApiKey}`;
+    // const ipstackApiKey = '3603411b489e490fe3f455f0bf710b76';
+    const ipstackUrl = `http://api.ipstack.com/${clientIp}?access_key=3603411b489e490fe3f455f0bf710b76`;
 
     let location = 'Unknown';
     try {
         const ipstackResponse = await axios.get(ipstackUrl);
-        location = await ipstackResponse.data.country_name || 'Nigeria';
+        location = await ipstackResponse.data.country_name;
     } catch (error) {
         console.error('Error fetching location:', error);
     }
 
-    // Replace with your OpenWeatherMap API key
-    const weatherApiKey = process.env.OPENWEATHER_API;
-    const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${weatherApiKey}`
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=a4f02e3b7db39236397be428fc8efcc0`
 
-    let temperature = '73';
+    let temperature = 'unknown';
     try {
         const weatherResponse = await axios.get(weatherUrl);
-        temperature = await weatherResponse.data.main.temp;
-        console.log(weatherResponse.data)
+        temperature = await weatherResponse.data.city.coord.lat;
+        location == null ? location = weatherResponse.data.city.name : location = location
     } catch (error) {
         console.error('Error fetching weather:', error);
     }
 
-    const greeting = await `hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`;
+    const greeting = `hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`;
 
     res.json({
         client_ip: clientIp,
